b58b959cd78d120e714fcb9fe1e72d5a26549698
DIOCIAN <kms@ndio.io>
2019-08-13 10:38:38 +0900

    # 뷰 반영

--- vue/index.php
+++ vue/index.php
@@ -10,7 +10,8 @@
     <!-- mobile setting end-->
 
     <link href="../css/reset.css" rel="stylesheet" type="text/css">
-    <script src="https://unpkg.com/vue"></script>
+    <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
     <style>
         div {
             margin:40px 40px;
@@ -20,107 +21,51 @@
 
 <body>
 <div id="app">
-    {{ message }}
+    <h1>Hello App!</h1>
+    <p>
+        <!-- use router-link component for navigation. -->
+        <!-- specify the link by passing the `to` prop. -->
+        <!-- <router-link> will be rendered as an `<a>` tag by default -->
+        <router-link to="/foo">Go to Foo</router-link>
+        <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view></router-view>
 </div>
-
-<div id="app-2">
-  <span v-bind:title="message">
-    내 위에 잠시 마우스를 올리면 동적으로 바인딩 된 title을 볼 수 있습니다!
-  </span>
-</div>
-
-<div id="app-3">
-    <p v-if="seen">이제 나를 볼 수 있어요</p>
-</div>
-
-<div id="app-4">
-    <ol>
-        <li v-for="todo in todos">
-            {{ todo.text }}
-        </li>
-    </ol>
-</div>
-
-<div id="app-5">
-    <p>{{ message }}</p>
-    <button v-on:click="reverseMessage">메시지 뒤집기</button>
-</div>
-
-<div id="app-6">
-    <p>{{ message }}</p>
-    <input v-model="message">
-</div>
-
-<div id="app-7">
-    <ol>
-        <todo-item
-                v-for="item in groceryList"
-                v-bind:todo="item"
-                v-bind:key="item.id">
-        </todo-item>
-    </ol>
-</div>
-
 <script>
-    var app = new Vue({
-        el: '#app',
-        data: {
-            message: '안녕하세요 Vue~~~~!'
-        }
+    // 0. If using a module system, call Vue.use(VueRouter)
+
+    // 1. Define route components.
+    // These can be imported from other files
+    const Foo = { template: '<div>foo</div>' }
+    const Bar = { template: '<div>bar</div>' }
+
+    // 2. Define some routes
+    // Each route should map to a component. The "component" can
+    // either be an actual component constructor created via
+    // Vue.extend(), or just a component options object.
+    // We'll talk about nested routes later.
+    const routes = [
+        { path: '/foo', component: Foo },
+        { path: '/bar', component: Bar }
+    ]
+
+    // 3. Create the router instance and pass the `routes` option
+    // You can pass in additional options here, but let's
+    // keep it simple for now.
+    const router = new VueRouter({
+        routes
     })
-    var app2 = new Vue({
-        el: '#app-2',
-        data: {
-            message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
-        }
-    })
-    var app3 = new Vue({
-        el: '#app-3',
-        data: {
-            seen: true
-        }
-    })
-    var app4 = new Vue({
-        el: '#app-4',
-        data: {
-            todos: [
-                { text: 'JavaScript 배우기' },
-                { text: 'Vue 배우기' },
-                { text: '무언가 멋진 것을 만들기' }
-            ]
-        }
-    })
-    var app5 = new Vue({
-        el: '#app-5',
-        data: {
-            message: '안녕하세요! Vue.js!'
-        },
-        methods: {
-            reverseMessage: function () {
-                this.message = this.message.split('').reverse().join('')
-            }
-        }
-    })
-    var app6 = new Vue({
-        el: '#app-6',
-        data: {
-            message: '안녕하세요 Vue!'
-        }
-    })
-    Vue.component('todo-item', {
-        props: ['todo'],
-        template: '<li>{{ todo.text }}</li>'
-    })
-    var app7 = new Vue({
-        el: '#app-7',
-        data: {
-            groceryList: [
-                { id: 0, text: 'Vegetables' },
-                { id: 1, text: 'Cheese' },
-                { id: 2, text: 'Whatever else humans are supposed to eat' }
-            ]
-        }
-    })
+
+    // 4. Create and mount the root instance.
+    // Make sure to inject the router with the router option to make the
+    // whole app router-aware.
+    const app = new Vue({
+        router
+    }).$mount('#app')
+
+    // Now the app has started!
 </script>
 </body>
 </html>
